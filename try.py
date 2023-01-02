4363c712a9187bf94196d46e2239bcc6c6734a7f
Roy-de <72699015+Roy-de@users.noreply.github.com>
2023-01-02 08:17:57 +0300

    change 1

--- try.py
+++ try.py
@@ -1,4 +1,15 @@
 from selenium import webdriver
-driver =  webdriver.Edge()
-driver.get("https://www.selenium.dev/documentation/webdriver/getting_started/install_drivers")
-assert ("selenium") in driver.title
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Edge()
+driver.get("http:/www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME,"q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No result found." not in driver.page_source
+time.sleep(30)
+driver.close()
