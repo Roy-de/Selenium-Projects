7ebe5168d69eb493456c9bf271eda8a120bf1993
Roy-de <72699015+Roy-de@users.noreply.github.com>
2023-01-02 09:33:41 +0300

    try 2

--- try.py
+++ try.py
@@ -4,12 +4,12 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Edge()
-driver.get("http:/www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME,"q")
+driver.get("https://finance.yahoo.com")
+#assert "Python" in driver.title
+elem = driver.find_element(By.NAME,"yfin-usr-qry")
 elem.clear()
-elem.send_keys("pycon")
+elem.send_keys("GBPUSD=X")
 elem.send_keys(Keys.RETURN)
-assert "No result found." not in driver.page_source
+#assert "No result found." not in driver.page_source
 time.sleep(30)
 driver.close()
